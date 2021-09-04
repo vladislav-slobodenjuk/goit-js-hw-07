910946e36a879417bd9d40e0dc71639061cdedc3
vladislav-slobodenjuk <vladjun@gmail.com>
2021-09-04 19:20:17 +0300

    task-1

---
+++ js/task-01.js
@@ -0,0 +1,13 @@
+const categories = document.querySelectorAll('#categories .item');
+// console.dir(categories);
+console.log(`В списке ${categories.length} категории.`);
+
+categories.forEach(topic =>
+    console.log(
+        `Категория: ${topic.querySelector('h2').textContent}, Количество элементов: ${
+            topic.querySelectorAll('li').length
+        }`,
+    ),
+);
+
+// categories.forEach(topic => console.dir(topic.querySelectorAll('li')));

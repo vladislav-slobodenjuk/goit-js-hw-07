c49afc5a0d166c4dbf2b35737d3915eaacc55c5a
vladislav-slobodenjuk <vladjun@gmail.com>
2021-09-04 21:53:52 +0300

    task-2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,12 @@
-const ingredients = [
-  'Картошка',
-  'Грибы',
-  'Чеснок',
-  'Помидоры',
-  'Зелень',
-  'Приправы',
-];
+const ingredients = ['Картошка', 'Грибы', 'Чеснок', 'Помидоры', 'Зелень', 'Приправы'];
+
+const ingredientsList = document.querySelector('#ingredients');
+// console.log(ingredientsList);
+
+const ingredientsToAdd = ingredients.map(ingredient => {
+    const ListElement = document.createElement('li');
+    ListElement.textContent = ingredient;
+    return ListElement;
+});
+
+ingredientsList.append(...ingredientsToAdd);

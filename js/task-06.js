59fb375b8ebdcf9c8d7799c6f19b30943cc12331
vladislav-slobodenjuk <vladjun@gmail.com>
2021-09-05 16:28:18 +0300

    task-6

--- js/task-06.js
+++ js/task-06.js
@@ -1,8 +1,21 @@
 const input = document.getElementById('validation-input');
 const length = input.getAttribute('data-length');
 
-console.log(length);
+// console.log(length);
 
 input.addEventListener('blur', onInputBlur);
 
-function onInputBlur(event) {}
+function onInputBlur(event) {
+  // console.log(input.value.length);
+  // console.log(event.currentTarget.value.length);
+
+  if (input.value.length == length) {
+    // console.log('верно 6');
+    input.classList.remove('invalid');
+    input.classList.add('valid');
+    return;
+  }
+  // console.log('не верно, не 6');
+  input.classList.remove('valid');
+  input.classList.add('invalid');
+}

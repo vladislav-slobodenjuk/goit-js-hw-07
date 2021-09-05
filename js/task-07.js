3ae77482afae265d92ffd474e5f955c8fa776056
vladislav-slobodenjuk <vladjun@gmail.com>
2021-09-05 20:41:06 +0300

    task-7

--- js/task-07.js
+++ js/task-07.js
@@ -3,16 +3,9 @@ const refs = {
   outPut: document.getElementById('text'),
 };
 
-refs.outPut.style.fontSize = '16px';
-console.log(refs.outPut.style.fontSize);
-
-// console.log(refs.outPut.style.fontSize);
-
 refs.rangeInput.addEventListener('input', onInputChange);
 
 function onInputChange(event) {
-  // console.log(event.target.style.fontSize);
-  // console.log(refs.rangeInput.value);
-  // console.log(refs.outPut.style.fontSize);
-  refs.outPut.style.fontSize = `${refs.rangeInput.value}px`;
+  refs.outPut.style.fontSize = `${refs.rangeInput.value}px`; // или `${event.target.valueAsNumber}px`
+  // console.log(event);
 }

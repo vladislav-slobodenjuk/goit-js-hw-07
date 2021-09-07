90710ef1963808c12482dc2d3becb91bbf5d1dc5
vladislav-slobodenjuk <vladjun@gmail.com>
2021-09-08 01:00:14 +0300

    task-6 fix

--- js/task-06.js
+++ js/task-06.js
@@ -1,5 +1,7 @@
 const input = document.getElementById('validation-input');
 const length = input.getAttribute('data-length');
+const valid = 'valid';
+const invalid = 'invalid';
 
 // console.log(length);
 
@@ -11,11 +13,14 @@ function onInputBlur(event) {
 
   if (input.value.length == length) {
     // console.log('верно 6');
-    input.classList.remove('invalid');
-    input.classList.add('valid');
+    changeClass(valid, invalid);
     return;
   }
   // console.log('не верно, не 6');
-  input.classList.remove('valid');
-  input.classList.add('invalid');
+  changeClass(invalid, valid);
+}
+
+function changeClass(add, remove) {
+  input.classList.add(add);
+  input.classList.remove(remove);
 }
